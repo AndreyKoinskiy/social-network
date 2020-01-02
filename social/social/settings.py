b655938a60d7b680bad15c1fd90d857cf620c249
Andrey Kosinskiy <kosinskiyandrey@ukr.net>
2020-01-02 12:03:25 +0200

    add and register Image model

--- social/social/settings.py
+++ social/social/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'django.contrib.admin',
     'bookmarks.apps.BookmarksConfig',
+    'images.apps.ImagesConfig',
 
     'social_django',
 ]
