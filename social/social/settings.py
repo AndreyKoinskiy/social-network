c89a13ae09b263b3c7e34fba263a42541d83bdce
Andrey Kosinskiy <kosinskiyandrey@ukr.net>
2020-01-09 17:15:21 +0200

    Adde ranking and couter of views for image with Redis

--- social/social/settings.py
+++ social/social/settings.py
@@ -162,3 +162,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'VNBEeNVHRtJPAa6LXdD_InmH' # Google Consumer 
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user' : lambda u: reverse_lazy('user_detail',args=[u.username])
 }
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
